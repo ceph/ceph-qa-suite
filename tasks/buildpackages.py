cd9459f24470eec2835cdd02b5a4c85262a4c435
Loic Dachary <ldachary@redhat.com>
2015-10-06 13:13:31 +0200

    buildpackages: honour install priorities tags, branch, sha1
    The install config may have contradicting tag/branch and sha1.  When
    suite.py prepares the jobs, it always overrides the sha1 with whatever
    default is provided on the command line with --distro and what is found
    in the gitbuilder. If it turns out that the tag or the branch in the
    install config task is about another sha1, it will override anyway.
    
    Instead of obtaining the tag, branch and sha1 directly from the
    packaging.GitbuilderProject object, compute them from the returned
    uri_reference data member. The uri_reference is used by the install task
    to fetch packages in the gitbuilders and this is what buildpackages
    needs to build.
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- tasks/buildpackages.py
+++ tasks/buildpackages.py
@@ -8,7 +8,8 @@ teuthology-openstack --verbose --key-name myself --key-filename ~/Downloads/myse
 """
 import logging
 import os
-import subprocess
+import re
+from subprocess import check_output, check_call
 from teuthology import packaging
 from teuthology import misc as teuthology
 from teuthology.config import config as teuth_config
@@ -17,6 +18,68 @@ from teuthology.openstack import OpenStack
 import urlparse
 
 log = logging.getLogger(__name__)
+
+def get_tag_branch_sha1(gitbuilder):
+    """
+    The install config may have contradicting tag/branch and sha1.
+    When suite.py prepares the jobs, it always overrides the sha1 with
+    whatever default is provided on the command line with --distro
+    and what is found in the gitbuilder. If it turns out that the
+    tag or the branch in the install config task is about another sha1,
+    it will override anyway. For instance:
+
+    install:
+       tag: v0.94.1
+
+    will be changed into
+
+    install:
+       tag: v0.94.1
+       sha1: 12345
+
+    even though v0.94.1 is not sha1 12345. This is does not cause
+    problem with the install task because packaging._get_uri_reference
+    is used to figure out what to install from the gitbuilder and this
+    function gives priority to the tag, if not found the branch, if
+    not found the sha1.
+
+    It is however confusing and this function returns a sha1 that is
+    consistent with the tag or the branch being used.
+    """
+
+    uri_reference = gitbuilder.uri_reference
+    url = gitbuilder.base_url
+    assert '/' + uri_reference in url, \
+        (url + ' (from template ' + teuth_config.baseurl_template +
+         ') does not contain /' + uri_reference)
+    log.info('uri_reference ' + uri_reference)
+    if uri_reference.startswith('ref/'):
+        ref = re.sub('^ref/', '', uri_reference) # do not use basename because the ref may contain a /
+        ceph_git_url = teuth_config.get_ceph_git_url()
+        cmd = "git ls-remote " + ceph_git_url + " " + ref
+        output = check_output(cmd, shell=True)
+        if not output:
+            raise Exception(cmd + " returns nothing")
+        lines = output.splitlines()
+        if len(lines) != 1:
+            raise Exception(
+                cmd + " returns " + output +
+                " which contains " + str(len(lines)) +
+                " lines instead of exactly one")
+        log.info(cmd + " returns " + lines[0])
+        (sha1, ref) = lines[0].split()
+        if ref.startswith('refs/heads/'):
+            tag = None
+            branch = re.sub('^refs/heads/', '', ref)
+        elif ref.startswith('refs/tags/'):
+            tag = re.sub('^refs/tags/', '', ref)
+            branch = None
+    else:
+        sha1 = os.path.basename(uri_reference)
+        tag = None
+        branch = None
+    return (tag, branch, sha1)
+
 
 def get_install_config(ctx):
     for task in ctx.config['tasks']:
@@ -67,26 +130,11 @@ def task(ctx, config):
         'task only accepts a dict for config not ' + str(config)
     d = os.path.join(os.path.dirname(__file__), 'buildpackages')
     install_config = get_install_config(ctx)
-    log.info('install_config ' + str(install_config))
     for remote in ctx.cluster.remotes.iterkeys():
         gitbuilder = install._get_gitbuilder_project(
             ctx, remote, install_config)
-        tag = packaging._get_config_value_for_remote(
-            ctx, remote, install_config, 'tag')
-        branch = packaging._get_config_value_for_remote(
-            ctx, remote, install_config, 'branch')
-        sha1 = packaging._get_config_value_for_remote(
-            ctx, remote, install_config, 'sha1')
-        uri_reference = gitbuilder.uri_reference
-        url = gitbuilder.base_url
-        assert '/' + uri_reference in url, \
-            (url + ' (from template ' + teuth_config.baseurl_template +
-             ') does not contain /' + uri_reference)
-        if 'ref/' in uri_reference:
-            ref = os.path.basename(uri_reference)
-        else:
-            ref = ''
-        subprocess.check_call(
+        (tag, branch, sha1) = get_tag_branch_sha1(gitbuilder)
+        check_call(
             "flock --close /tmp/buildpackages " +
             "make -C " + d + " " + os.environ['HOME'] + "/.ssh_agent",
             shell=True)
@@ -110,13 +158,12 @@ def task(ctx, config):
                " CEPH_SHA1=" + (sha1 or '')  +
                " CEPH_TAG=" + (tag or '') +
                " CEPH_BRANCH=" + (branch or '') +
-               " CEPH_REF=" + ref +
                " GITBUILDER_URL=" + url +
                " BUILD_FLAVOR=" + build_flavor +
                " HTTP_FLAVOR=" + http_flavor +
                " " + target +
                " ")
         log.info("buildpackages: " + cmd)
-        subprocess.check_call(cmd, shell=True)
+        check_call(cmd, shell=True)
     teuth_config.gitbuilder_host = openstack.get_ip('packages-repository', '')
     log.info('Finished buildpackages')
