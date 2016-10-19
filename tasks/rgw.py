45211b36939f8058b1cce9128a6ae421560cdb4b
Ali Maredia <amaredia@redhat.com>
2016-10-19 17:52:18 -0400

    rgw multisite: added zone that was accidently taken out
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- tasks/rgw.py
+++ tasks/rgw.py
@@ -816,7 +816,7 @@ def configure_multisite_regions_and_zones(ctx, config, regions, role_endpoints, 
              check_status=True)
 
     rgwadmin(ctx, master_client,
-             cmd=['zone', 'default', '--rgw-zone', '--cluster', cluster_name],
+             cmd=['zone', 'default', '--rgw-zone', zone, '--cluster', cluster_name],
              check_status=True)
 
     rgwadmin(ctx, master_client,
